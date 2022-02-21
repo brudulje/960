e44ab61f8640183cdda29f9c83074e457f2bbd7d
Joachim Seland Graff <Joachim.Seland.Graff@gmail.com>
2022-02-21 21:41:43 +0100

    Added options verbose and language.
    The starting position may now be dispalyed using the short names
    (single letter) of hte chess pieces in several languages:
    - Norwegian
    - English
    - Finninsh
    - French
    
    Language settings are read from languages.json. Languages may be
    added directly in the .json file.

--- start960.py
+++ start960.py
@@ -6,10 +6,43 @@ Created on Wed Nov  3 20:48:54 2021
 """
 
 import numpy as np
+import argparse
+# import csv
+import json
 """
 Generate and print a random, legal, 
 starting position for chess 960.
 """
+parser = argparse.ArgumentParser()
+parser.add_argument("-v", "--verbose", help="increase output verbosity",
+                    action="store_true")
+parser.add_argument("-l", "--language", type=str,# choices=["no", "en"],
+                    default="no",
+                    help="Choose language",)
+args = parser.parse_args()
+
+# Read languages from file:
+with open("languages.json", "r") as infile:
+    lang = json.load(infile)
+lang_found = False
+
+# Set language
+for k, v, in lang.items():
+    if args.language == k:
+        lang_found = True
+        language = v
+        
+# Set backup language
+if not lang_found:
+    language = lang["no"]
+    print(f"Language '{args.language}' not found, defaulting to Norwegian.")
+
+king = language[0]
+queen = language[1]
+rook = language[2]
+bishop = language[3]
+knight = language[4]
+hilsen = language[5]
 
 
 def replace_char(s, char="", index=0):
@@ -17,48 +50,39 @@ def replace_char(s, char="", index=0):
     return s[:index] + char + s[index + 1:]
 
 
-king = "K"
-queen = "D"
-rook = "T"
-bishop = "L"
-knight = "S"
-hilsen = "Lykke til!"
-
-lisemin = "<3"
 s = "abcdefgh"
-verbose = False
 
-if verbose:
+if args.verbose:
     print(" ", s)
 
 n = np.random.randint(4)  # Light-squared bishop
 s = replace_char(s, bishop, (2 * n) + 1)
-if verbose:
+if args.verbose:
     print(n, s)
 
 n = np.random.randint(4)  # Dark-squared bishop
 s = replace_char(s, bishop, (2 * n))
-if verbose:
+if args.verbose:
     print(n, s)
 
 n = np.random.randint(6)  # Queen
 # Getting the still available positions
 pos = [i for i, n in enumerate(s) if n in 'abcdefgh']
 s = replace_char(s, queen, pos[n])
-if verbose:
+if args.verbose:
     print(n, s)
 
  
 n = np.random.randint(5)  # First Knight
 pos = [i for i, n in enumerate(s) if n in 'abcdefgh']
 s = replace_char(s, knight, pos[n])
-if verbose:
+if args.verbose:
     print(n, s)
 
 n = np.random.randint(4)  # Second  Knight
 pos = [i for i, n in enumerate(s) if n in 'abcdefgh']
 s = replace_char(s, knight, pos[n])
-if verbose:
+if args.verbose:
     print(n, s)
 
 # Rook, King and Rook
